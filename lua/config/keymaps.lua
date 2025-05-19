b715b1b419beedc6650c60ca060015621fa3a58e
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2025-05-19 04:20:37 -0400

    Make terminal remember buffer number between toggle

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,14 +14,31 @@ map("n", "<leader><tab>c", "<cmd>BufferLineCloseRight<cr>", { desc = "Close Othe
 map("n", "<leader><S-tab>c", "<cmd>BufferLineCloseLeft<cr>", { desc = "Close Other Tabs" })
 
 -- Terminal
-local lastNumber = 1
+local actionNumber = 1
+
 map("n", "1", function()
-  lastNumber = 2
-end, { desc = "Terminal" })
+  actionNumber = 1
+end, { desc = "Set action number to 1" })
 map("n", "2", function()
-  lastNumber = 2
-end, { desc = "Terminal" })
+  actionNumber = 2
+end, { desc = "Set action number to 2" })
+map("n", "3", function()
+  actionNumber = 3
+end, { desc = "Set action number to 3" })
+map("n", "4", function()
+  actionNumber = 4
+end, { desc = "Set action number to 4" })
 
 map("n", "<c-/>", function()
-  Snacks.terminal(nil, { cwd = LazyVim.root() })
-end, { desc = "Terminal" })
+  print("Number: " .. actionNumber)
+  Snacks.terminal
+    .toggle(nil, { cwd = LazyVim.root(), win = { position = "float" }, env = { id = "term" .. actionNumber } })
+    :add_padding()
+end, { desc = "Terminal float toggle base on action number" })
+
+map("n", "<c-\\>", function()
+  print("Number: " .. actionNumber)
+  Snacks.terminal
+    .toggle(nil, { cwd = LazyVim.root(), win = { position = "bottom" }, env = { id = "term" .. actionNumber } })
+    :add_padding()
+end, { desc = "Terminal bottom toggle base on action number" })
