506cfe02c0876daa8ec8bfa99a145e2297a5aeff
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2025-05-25 18:00:54 -0400

    Change dashboard

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -8,18 +8,23 @@ return {
       end
       opts.lualine_bold = true
       opts.on_highlights = function(hl, c)
+        -- Here is Tokyonight's default colors (https://github.com/folke/tokyonight.nvim/blob/main/lua/tokyonight/colors/moon.lua)
         hl["SnacksDashboardHeader"] = {
           fg = choose({
-            c.green,
-            c.magenta,
-            c.orange,
-            c.purple,
-            c.red,
+            -- c.green,
+            -- c.magenta,
+            -- c.orange,
+            -- c.purple,
+            -- c.red,
             c.yellow,
           }),
         }
+        -- Use that following command to see the available highlights
+        -- Snacks.picker.highlights({pattern = "hl_group:^SnacksDashboard"})
         hl["SnacksDashboardTitle"] = { fg = c.magenta, bold = true }
-        hl["SnacksDashboardKey"] = { fg = c.orange, bold = true }
+        hl["SnacksDashboardIcon"] = { fg = c.yellow, bold = true }
+        hl["SnacksDashboardDesc"] = { fg = c.fg, bold = true }
+        hl["SnacksDashboardKey"] = { fg = c.yellow, bold = true }
       end
       return opts
     end,
