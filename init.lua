595b2f98c6abbb9717f5a117493038c5f12467af
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2025-05-28 11:38:02 -0400

    Fix a React Component comments issue & update plugins

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Use to enable context comments like React component templating comments
+local get_option = vim.filetype.get_option
+---@diagnostic disable-next-line: duplicate-set-field
+vim.filetype.get_option = function(filetype, option)
+  return option == "commentstring" and require("ts_context_commentstring.internal").calculate_commentstring()
+    or get_option(filetype, option)
+end
