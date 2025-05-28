1af1964206de2f08e2c31f2ef735b9f4d8970f64
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2025-05-28 19:07:01 -0400

    Fix comments function

--- init.lua
+++ init.lua
@@ -2,9 +2,16 @@
 require("config.lazy")
 
 -- Use to enable context comments like React component templating comments
+local function calculate_commentstring()
+  return require("ts_context_commentstring.internal").calculate_commentstring()
+end
+
 local get_option = vim.filetype.get_option
 ---@diagnostic disable-next-line: duplicate-set-field
 vim.filetype.get_option = function(filetype, option)
-  return option == "commentstring" and require("ts_context_commentstring.internal").calculate_commentstring()
-    or get_option(filetype, option)
+  if pcall(calculate_commentstring) then
+    return calculate_commentstring()
+  else
+    return get_option(filetype, option)
+  end
 end
