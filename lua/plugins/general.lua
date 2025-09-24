f82d6c19857f43283f490701f693d1b83d94d35b
t1gu1 <guillaumehuardhughes@gmail.com>
2025-09-24 01:47:28 -0400

    Fix warnings
    Fix some warning since the update when opening nvim

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -156,17 +156,20 @@ return {
       local package_info = require("package-info")
 
       require("package-info").setup({
-        colors = {
-          up_to_date = "#3C4048", -- Text color for up to date dependency virtual text
-          outdated = "#d19a66", -- Text color for outdated dependency virtual text
+        highlights = {
+          up_to_date = { fg = "#3C4048" },
+          outdated = { fg = "#d19a66" },
+          invalid = { fg = "#ee4b2b" },
         },
         icons = {
           enable = true, -- Whether to display icons
           style = {
             up_to_date = "|  ", -- Icon for up to date dependencies
             outdated = "|  ", -- Icon for outdated dependencies
+            invalid = "|  ", -- Icon for invalid dependencies
           },
         },
+        notifications = true, -- Whether to display notifications when running commands
         autostart = true, -- Whether to autostart when `package.json` is opened
         hide_up_to_date = false, -- It hides up to date versions when displaying virtual text
         hide_unstable_versions = false, -- It hides unstable versions from version list e.g next-11.1.3-canary3
