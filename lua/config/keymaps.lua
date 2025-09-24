bb2d0d9ce9db9ac4336c297065d06714a4feb69a
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2025-09-24 19:23:05 -0400

    Refactors keymaps for improved readability
    Consolidates and streamlines keymap definitions.
    
    Uses loops to define delete/change without yanking, and move line keymaps, reducing redundancy and improving maintainability.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,17 +5,24 @@
 local map = vim.keymap.set
 local unmap = vim.keymap.del
 
-unmap("n", "<c-/>") -- Unmap old way to toggle terminal
+-- Unmap old way to toggle terminal
+unmap("n", "<c-/>")
 
+-- Macro trigger
 map({ "n", "v" }, ";", "@q", { desc = "Trigger macro in register q" })
 
--- Will not remove the yank when replacing or removing
-map({ "n", "v" }, "d", '"_d', { desc = "Delete without remove yank" })
-map({ "n", "v" }, "D", '"_D', { desc = "Delete without remove yank" })
-map({ "n", "v" }, "c", '"_c', { desc = "Replace without remove yank" })
-map({ "n", "v" }, "C", '"_C', { desc = "Replace without remove yank" })
-map("n", "x", '"_x', { desc = "Remove without remove yank" })
-map("n", "X", '"_X', { desc = "Remove without remove yank" })
+-- Delete/Change without yanking
+for _, key in ipairs({ "d", "D", "c", "C" }) do
+  map(
+    { "n", "v" },
+    key,
+    '"_' .. key,
+    { desc = (key:lower() == "d" and "Delete" or "Replace") .. " without remove yank" }
+  )
+end
+for _, key in ipairs({ "x", "X" }) do
+  map("n", key, '"_' .. key, { desc = "Remove without remove yank" })
+end
 
 -- Indents
 map("i", "<Tab>", "<cmd>><cr>", { desc = "Indent" })
@@ -26,22 +33,19 @@ map("v", "<S-Tab>", "<gv", { desc = "Remove an indent to a selection" })
 -- Search
 map("n", "<leader>d/", "<cmd>noh<cr>", { desc = "Turn off highlighting until the next search" })
 
+-- File pickers
 map({ "n", "v" }, "<leader>ff", function()
   Snacks.picker.files({ hidden = true })
 end, { desc = "Find a file" })
-
 map({ "n", "v" }, "<leader>fw", function()
   Snacks.picker.grep({ regex = false, hidden = true })
 end, { desc = "Find a words / text" })
-
 map({ "n", "v" }, "<leader>f<leader>", function()
   Snacks.picker.resume()
 end, { desc = "Find a words / text" })
-
 map({ "n", "v" }, "<leader>fr", function()
   require("grug-far").open()
 end, { desc = "Find and replace" })
-
 map({ "n", "v" }, "<leader>ft", function()
   vim.api.nvim_feedkeys(" st", "m", false)
 end, { desc = "Find TODO in the project" })
@@ -50,7 +54,6 @@ end, { desc = "Find TODO in the project" })
 map("n", "<leader>e", function()
   Snacks.explorer.open()
 end, { desc = "Explorer - Reveal active file" })
-
 map("n", "<leader>E", function()
   Snacks.explorer.open({
     follow_file = false,
@@ -61,9 +64,8 @@ map("n", "<leader>E", function()
 end, { desc = "Explorer - Reveal root project" })
 
 -- Copilot
--- Other keymaps for Copilot are in general under the plugin "zbirenbaum/copilot.lua"
-map({ "i" }, "<c-right>", "<cmd>Copilot suggestion accept_line<cr>", { desc = "Select all" })
-map({ "i" }, "<c-left>", "<cmd>Copilot suggestion dismiss<cr>", { desc = "Select all" })
+map("i", "<c-right>", "<cmd>Copilot suggestion accept_line<cr>", { desc = "Select all" })
+map("i", "<c-left>", "<cmd>Copilot suggestion dismiss<cr>", { desc = "Select all" })
 
 -- Code
 map({ "n", "v" }, "<c-a>", "ggvG", { desc = "Select all" })
@@ -74,30 +76,30 @@ map({ "n", "v" }, "<c-f>", function()
   vim.api.nvim_feedkeys(" cM", "m", false)
 end, { desc = "Remove unused import and add missing imports" })
 
--- Increment and Decrement a number
--- Go see in the `general.lua` file
-
--- Move Lines
-map("n", "<S-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
-map("n", "<S-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
-map("n", "<S-DOWN>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
-map("n", "<S-UP>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
-map(
-  "i",
-  "<S-DOWN>",
-  "<cmd>stopinsert<cr><cmd>execute 'move .+' . v:count1<cr>==<cmd>lua vim.api.nvim_input('a')<cr>",
-  { desc = "Move Down" }
-)
-map(
-  "i",
-  "<S-UP>",
-  "<cmd>stopinsert<cr><cmd>execute 'move .-' . (v:count1 + 1)<cr>==<cmd>lua vim.api.nvim_input('a')<cr>",
-  { desc = "Move Up" }
-)
-map("v", "<S-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
-map("v", "<S-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
-map("v", "<S-DOWN>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
-map("v", "<S-UP>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+-- Move Lines (Normal, Insert, Visual)
+local move_mappings = {
+  n = {
+    ["<S-j>"] = "<cmd>execute 'move .+' . v:count1<cr>==",
+    ["<S-k>"] = "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==",
+    ["<S-DOWN>"] = "<cmd>execute 'move .+' . v:count1<cr>==",
+    ["<S-UP>"] = "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==",
+  },
+  i = {
+    ["<S-DOWN>"] = "<cmd>stopinsert<cr><cmd>execute 'move .+' . v:count1<cr>==<cmd>lua vim.api.nvim_input('a')<cr>",
+    ["<S-UP>"] = "<cmd>stopinsert<cr><cmd>execute 'move .-' . (v:count1 + 1)<cr>==<cmd>lua vim.api.nvim_input('a')<cr>",
+  },
+  v = {
+    ["<S-j>"] = ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv",
+    ["<S-k>"] = ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv",
+    ["<S-DOWN>"] = ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv",
+    ["<S-UP>"] = ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv",
+  },
+}
+for mode, tbl in pairs(move_mappings) do
+  for k, v in pairs(tbl) do
+    map(mode, k, v, { desc = "Move " .. (k:find("j") or k:find("DOWN") and "Down" or "Up") })
+  end
+end
 
 -- Tabs
 map("n", "<tab>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Tab" })
@@ -113,7 +115,7 @@ map("n", "<leader><S-tab>", "<cmd>BufferLineMovePrev<cr>", { desc = "Move buffer
 -- There is other keymaps for terminal in dashboard-and-explorer.lua
 local lastTerminalNumberOpened = 1
 
-function OpenTerminal()
+local function OpenTerminal()
   Snacks.terminal.toggle(nil, {
     cwd = LazyVim.root(),
     win = { position = "float", border = "rounded", title = "#" .. lastTerminalNumberOpened },
@@ -125,7 +127,7 @@ for id = 1, 9 do
   map("n", id .. "<leader>t", function()
     lastTerminalNumberOpened = id
     OpenTerminal()
-  end, { desc = "Terminal float opening base on the number #" .. lastTerminalNumberOpened })
+  end, { desc = "Terminal float opening base on the number #" .. id })
 end
 
 map("n", "<leader>t", function()
